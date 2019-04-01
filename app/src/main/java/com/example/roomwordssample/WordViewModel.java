5e940e33187306bb8ede68db35043ac12459f3b9
Wong Jun Siong <wongjs-wm17@student.tarc.edu.my>
2019-04-01 18:26:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/roomwordssample/WordViewModel.java
@@ -0,0 +1,22 @@
+package com.example.roomwordssample;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+    private WordRepository mRepository;
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(Application application){
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getmAllWords();
+    }
+
+    LiveData<List<Word>> getmAllWords() {return mAllWords;}
+
+    public void insert(Word word){mRepository.insert(word);}
+}
